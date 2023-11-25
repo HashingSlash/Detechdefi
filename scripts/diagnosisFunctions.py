fcbd0fb9669634444400e3ba2cd2881665023bc0
HashingSlash <dcjpreece@protonmail.com>
2023-11-25 19:34:39 +0800

    clean and comments

--- scripts/diagnosisFunctions.py
+++ scripts/diagnosisFunctions.py
@@ -4,22 +4,27 @@ import scripts.databaseFunctions as databaseFunctions
 import scripts.transactionFunctions as transactionFunctions
 
 def buildPlatformDiagDB(requestedPlatform):
+    #this builds the diag dictionary used for parsing group txn entries.
+    #platform files are managed individually for readability
+    #Platform files to load
     platforms = ['Algofi', 'Yieldly', 'AlgoFund', 'Folks Finance', 'Pact', 'Tinyman', 'Vestige', 'Deflex']
     diagDB = {}
+    #This allows for working on only a single Platform. Saves compute while testing.
     if requestedPlatform in platforms:
         platforms = [requestedPlatform]
+    #if no platform soloed, load them all.
     for platformToLoad in platforms:
         try:
             inFile = open('platforms/' + platformToLoad + '.json', 'r')
             diagDB[platformToLoad] = json.load(inFile)
             inFile.close()
-            
         except IOError:
             print('Diag IOError : ' + str(platformToLoad))
 
     return diagDB
 
-
+#keypair for tx-type and txn subdictionary title.
+#theres a better home for this.
 txnTypeDetail = {'acfg' : 'asset-config-transaction',
                 'afrz' : 'asset-freeze-transaction',
                 'appl' : 'application-transaction',
@@ -28,7 +33,7 @@ txnTypeDetail = {'acfg' : 'asset-config-transaction',
                 'pay' : 'payment-transaction',
                 'stpf' : 'state-proof-transaction'}
 
-
+#quick decode script for txn notes, app args, so on.
 def base64decode(toDecode):
     try:
         #print(toDecode)
@@ -42,11 +47,12 @@ def base64decode(toDecode):
 
 
 def diagDBParse(mainDB, groupID, diagDB):
-
+    #fresh dictionary for group diagnosis process.
+    #starts fresh each time to keep definitions up to date
     #Grab IDs, onCompletion and application arguments from txns
-    tempDB = {"groupAppIDs" : [],
-        "groupAppArgs" : [],
-        "groupOnComplete" : [],
+    tempDB = {"groupAppIDs" : [], #all app IDs and foreign app IDs.
+        "groupAppArgs" : [],#each application argument from each application call txn in the group
+        "groupOnComplete" : [],#all 'on-completion' data from txn type subdictionaries.
         "groupPlatforms" : [],
         "groupAppGroups" : [],
         "groupActions" : []}
@@ -54,17 +60,20 @@ def diagDBParse(mainDB, groupID, diagDB):
     for txnID in mainDB['groups'][groupID]['txns']:
         #load txn main body and txn-type unique body into vars
         txn = mainDB['rawTxns'][txnID]
+        #txnTypeDetails is the txn subdictionary relevant to the txn type. Payment, Asset xfer, so on.
         txnTypeDetails = txn[txnTypeDetail[txn['tx-type']]]
         #check application txns for details for diag
         if txn['tx-type'] == 'appl':
+            #this gets app and foreign app IDs
             txnAppID = txnTypeDetails['application-id']
             tempDB['groupAppIDs'] = databaseFunctions.appendToEmbeddedList(tempDB['groupAppIDs'], txnAppID)
             try:
                 for foreignAppID in txnTypeDetails['foreign-apps']:
                     tempDB['groupAppIDs'] = databaseFunctions.appendToEmbeddedList(tempDB['groupAppIDs'], foreignAppID)
             except:pass
-
+            #stores any unique 'on-complete' data
             tempDB['groupOnComplete'] = databaseFunctions.appendToEmbeddedList(tempDB['groupOnComplete'], txnTypeDetails['on-completion'])
+            #stores any app call arguments.
             if txnTypeDetails['application-args'] != []:
                 for txnAppArg in txnTypeDetails['application-args']:
                     tempDB['groupAppArgs'] = databaseFunctions.appendToEmbeddedList(tempDB['groupAppArgs'], txnAppArg)
@@ -77,12 +86,11 @@ def diagDBParse(mainDB, groupID, diagDB):
                     appGroupMatches = []
                     actionMatches = []
                     
-                    #check each app group, load its IDS, then iterate through its functions
+                    #check each app group in the DiagDB, load its IDS, then iterate through its functions
                     actionIDs = diagDB[testPlatform][testAppGroup]['IDs']
                     for txnGroupID in tempDB['groupAppIDs']:
                         #check for ID matches between this app group and the app IDs found in the Transaction group
                         if str(txnGroupID) in actionIDs:
-                            ##print('ID Match')
                             #If an ID match between the App IDs from the txn group, and the Platforms App group, store the platforms name
                             if testPlatform not in IDMatches: IDMatches.append(testPlatform)
                     #check each action within each app group.
